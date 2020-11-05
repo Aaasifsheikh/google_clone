ccda677ce6ccae03e5dca6ef1250adfedfd701a8
Aasif Sheikh <Aasifsheikh820@gmail.com>
2020-11-05 16:35:52 +0530

    Updates

---
+++ precache-manifest.613092d259a1ddfed0e8cd12acee9445.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02dcf8a7bf2d7ff727093c08ea785862",
+    "url": "/google_clone/index.html"
+  },
+  {
+    "revision": "620c0dd8ddf2dad76391",
+    "url": "/google_clone/static/css/main.bcdbff02.chunk.css"
+  },
+  {
+    "revision": "2826714e4a8b4d1536cc",
+    "url": "/google_clone/static/js/2.dfc960a2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/google_clone/static/js/2.dfc960a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620c0dd8ddf2dad76391",
+    "url": "/google_clone/static/js/main.7f41128d.chunk.js"
+  },
+  {
+    "revision": "8e35be88e9e3aea74eab",
+    "url": "/google_clone/static/js/runtime-main.4fe6abac.js"
+  },
+  {
+    "revision": "09cf36555906867b2e702444a4f366fb",
+    "url": "/google_clone/static/media/2WhatsApp Image 2020-10-08 at 3.09.05 PM.09cf3655.jpeg"
+  }
+]);
